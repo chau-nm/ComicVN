1ba55f1875b474a866c261177f0863a08d311868
Nguyen Minh Châu <91469379+light1406@users.noreply.github.com>
2022-07-28 23:20:29 +0700

    insert splash activity

---
+++ app/src/main/java/com/example/comicvn/ui/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.comicvn.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.comicvn.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1500);
+    }
+}
